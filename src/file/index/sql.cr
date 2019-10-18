c815d363ee383d7b3e96dfcf023f6dff13de80be
Paul M. Lambert <plambert@plambert.net>
2019-10-18 10:25:44 -0700

    make File::Index::SQL methods be class methods, as intended

--- src/file/index/sql.cr
+++ src/file/index/sql.cr
@@ -5,11 +5,11 @@ class File
     class SQL
       @@schema : String = {{read_file("src/file/index/schema.sql")}}
 
-      def schema
-        @@schema.split(%r{;[ \t]*\r?\n}).map { |sql| "#{sql};" }
+      def self.schema
+        @@schema.split(%r{(?<=;)[ \t]*\r?\n\s*}).select { |stmt| stmt =~ /\S/ }
       end
 
-      def apply(db : Database::DB)
+      def self.apply(db : Database::DB)
         self.schema.each do |statement|
           result = db.exec statement
         end
